951fd993e945dcf8e8153e3e5118759ace922321
KN1226 <44549750+kareem-mahmoud@users.noreply.github.com>
2020-10-26 14:16:26 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            number: 0
+        }
+    },
+    computed: {
+        result() {
+            if(this.number === 37) {
+                return this.number;
+            }else if(this.number < 37) {
+                return 'Not there yet';
+            }else {
+                return 'Too much!';
+            }
+        }
+    },
+    watch: {
+        result() {
+            const that = this;
+            setTimeout(function() {
+                that.number = 0;
+            }, 5000);
+        }
+    },
+    methods: {
+        addNum(num) {
+            this.number = this.number + num;
+        }
+    }
+}).mount('#assignment');
